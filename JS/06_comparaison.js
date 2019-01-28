6f1500d2552d4ce99a46180d539db447f8a999cb
DjalmilaChabane <djamila.chabane@lepoles.com>
2019-01-28 16:54:18 +0100

    front

--- JS/06_comparaison.js
+++ JS/06_comparaison.js
@@ -25,21 +25,56 @@
  en cas d'echecs une alerte m'informe du problème.
  si tout se passe bien, un message de bienvenue m'acceuille*/
 
-var monPrenom = "djamila";
-    monAge = 38;
-    var MDP = "Emma2";
- var prenom=  prompt( "veuillez saisir votre prénom");
- var age  = prompt( "veuillez saisir votre age");
-var motDePasse= prompt( " saisir le mot de passe");
+// var monPrenom = "djamila";
+//     monAge = 38;
+//     var MDP = "Emma2";
+//  var prenom=  prompt( "veuillez saisir votre prénom");
+//  var age  = prompt( "veuillez saisir votre age");
+// var motDePasse= prompt( " saisir le mot de passe");
  
-   if( prenom===monPrenom && age===monAge && motDePasse===MDP )
-   {alert( " vous etes le bienvenu"); } 
-else {   
-         alert(" vous n'etes pas authentifié");
-         }
+//    if( prenom===monPrenom && age===monAge && motDePasse===MDP )
+//    {alert( " vous etes le bienvenu"); } 
+// else {   
+//          alert(" vous n'etes pas authentifié");
+//          }
    
            
-  
+if(prenomLogin === prenom) {
+    // alert('test')
+    // 2a. Si tout est ok, je continue la vérification avec l'age.
+    //2a1. Je demande à mon utilisateur de rentrer son age (prompt)
+    var ageLogin = parceInt (prompt('Votre age?'));
+    if(ageLogin === monAge) {
+        alert('Bienvenue' + prenomLogin);
+    }else {
+        // 2a2. Si les ages ne correspondent pas, je lance une ALERT.
+        alert("Erreur d'age");
+    }
+ }else {
+    // 2b. Sinon, les prenoms ne correspondent pas, je lance une alerte.
+    alert("Attention, prénom non reconnu");
+ }
+ 
+ var prenom, monAge;
+ prenom = "Iuliia";
+ monAge = 24;
+ mdp = 'herisson';
+ var prenomLogin = prompt('Quelle est votre prénom?');
+ if(prenomLogin === prenom) {
+    var ageLogin = parseInt(prompt('Votre age?'));
+    if(ageLogin === monAge) {
+        var mdpLogin = prompt('Quelle est votre mot de passe?');
+        if(mdpLogin === mdp){
+            alert('bienvenue' + prenomLogin);
+        }else{
+            alert('Problème avec votre mot de passe!');
+        }
+    }alse{
+        alert('Erreur dage');
+    }
+ }else{
+    alert('Attention, prénom est pas reconnue');
+ }
   
 
 
