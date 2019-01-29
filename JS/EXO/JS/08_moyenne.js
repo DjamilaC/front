3edb03b1b36c8e1c954b36f7be50743c8693f605
DjalmilaChabane <45996340+DjamilaC@users.noreply.github.com>
2019-01-29 22:16:32 +0100

    front

--- JS/EXO/JS/08_moyenne.js
+++ JS/EXO/JS/08_moyenne.js
@@ -5,5 +5,7 @@ if( moy < 10){
     document.write("l'etudiant est: recalé ");
 }else if( moy >= 10 && moy < 12){
     document.write("l'etudiant est: reçu ");
-}else if( moy >= 12)
+}else if( moy >= 12 && moy<= 20){ 
 document.write("l'etudiant est: reçu avec mention ");
+ } else{ document.write(" la moyenne est invalide"); 
+}
