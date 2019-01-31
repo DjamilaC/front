ad3b0a26d9f9812086b60acceaf45bf19fb479be
DjalmilaChabane <djamila.chabane@lepoles.com>
2019-01-31 16:30:23 +0100

    front

---
+++ JS/EXO/JS/03_exo.js
@@ -0,0 +1,31 @@
+alert("bonjour");
+var a = 3;
+var b = 2;
+
+
+ function multiplie (x = 8){
+  return  3 * x ;   
+}
+function affiche(){
+  alert(multiplie(a));
+  alert(multiplie (b));
+  alert(multiplie ());
+}
+   
+   
+
+
+// var a= prompt("donner une valeur à a: ");
+// var resultat = multiplie * a;
+
+
+//     console.log( resultat)
+//     b= prompt(" donner une valeur à b");
+//     var resultat = multiplie * b;
+
+//     console.log( resultat)
+
+
+
+
+
